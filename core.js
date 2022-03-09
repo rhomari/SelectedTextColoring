28d9c5f9ed6b91c2a097a34331dfe56e91fa9b61
TARIQ RHOMARI <tariqrhomari@gmail.com>
2022-03-09 20:34:42 +0100

    htmlselection_fbchallenge

---
+++ core.js
@@ -0,0 +1,23 @@
+
+const filediag= document.getElementById('openfilediag')
+filediag.addEventListener("change", (e)=>{
+    let filereader = new FileReader()
+    filereader.onload = ()=>{
+        document.getElementById('filecontentviewer').innerText=  filereader.result
+    }
+    filereader.readAsText(e.target.files[0]);
+})
+function turnRed(){
+    
+    
+    
+    let selection= window.getSelection() 
+    const selRange= selection.getRangeAt(0).cloneRange()
+
+    const docFragment = selRange.extractContents()
+    console.log(docFragment)
+    let newnode = document.createElement("redmark")
+    newnode.appendChild(docFragment)
+    selRange.insertNode(newnode)
+    
+}
